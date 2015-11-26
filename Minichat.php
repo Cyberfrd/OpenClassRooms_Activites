0375547092a619159737d8befde2c1e251ad45bb
Cyberfrd <cyberfrd@hotmail.com>
2015-11-26 11:37:35 +0100

    Ajout du fihier minichat.php

---
+++ Minichat.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mini-chat</title>
+    </head>
+    <style>
+    form
+    {
+        text-align:center;
+    }
+    </style>
+    <body>
+    
+    <form action="minichat_post.php" method="post">
+        <p>
+        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" <?php if (isset($_GET['pseudo'])) { echo ' value="' . $_GET['pseudo'] . '"'; }?> /><br />
+        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+
+        <input type="submit" value="Envoyer" />
+    </p>
+    </form>
+
+    
+    </body>
+</html>
